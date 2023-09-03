aea49b1d537deada052d0e5101a69c4550119ef8
arfaoui chokri <arfaouichokri601@gmail.com>
2023-09-03 23:47:26 +0100

    big_inversion

---
+++ w1_day5/big_inversion/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import  PersonCard  from "./components/PersonCard";
+function App() {
+    const people = [
+        { firstName: "Jane", lastName: "Doe", age: 45, hairColor: "Black" },
+        { firstName: "John", lastName: "Smith", age: 88, hairColor: "Brown" },
+        { firstName: "Millard", lastName: "Fillmore",age: 50, hairColor: "Brown"},
+        { firstName: "Maria", lastName: "Smith", age: 62, hairColor: "Brown" },
+    ];
+
+    return (
+        <div className="App">
+            {people.map((person, index) => {
+                return <PersonCard key={index} data={person} />;
+            })}
+        </div>
+    );
+}
+
+export default App;
+
